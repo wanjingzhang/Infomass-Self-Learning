25e2218b60a482b9b0cbd3ead6359c1907b7c9f8
wanjingzhang <95616085@qq.com>
2020-05-05 21:17:47 +0800

    add the 3 types of drag

--- tableTree/js/data.js
+++ tableTree/js/data.js
@@ -1,4 +1,4 @@
-const vTableData={
+var vTableData={
      data: [
         {"deviceNo":"0001","deviceName":"百分表","status":"enable","manufacture":"成都成量工具集团",
         "located":"成都","nextMaintenanceDate":"2020/8/1","nextCalibrationDate":"2020/10/1",
@@ -13,7 +13,17 @@ const vTableData={
         "sparePart":1,"calibrationSupplier":"重庆测试研究院","toolSpecification":"(0-10)mm","procurementPrice":"¥300.00"
         }
     ],
-    tableHeader: {"工具编号":"deviceNo", "工具名称":"deviceName", "状态":"status", "制造商":"manufacture",
-    "存放架/工位":"located", "下次保养时间":"nextMaintenanceDate", "下次校准时间":"nextCalibrationDate", 
-    "备件":"sparePart", "校准服务提供商":"calibrationSupplier", "工具规格":"toolSpecification", "采购价格":"procurementPrice"}
+    tableHeader: [
+         {"key": "deviceNo", "value": "1.工具编号"}, 
+         {"key": "deviceName", "value": "2.工具名称"}, 
+         {"key": "status", "value":"3.状态"}, 
+         {"key": "manufacture", "value":"4.制造商"},
+         {"key": "located", "value":"5.存放架/工位"}, 
+         {"key": "nextMaintenanceDate", "value":"6.下次保养时间"}, 
+         {"key": "nextCalibrationDate", "value":"7.下次校准时间"}, 
+         {"key": "sparePart", "value":"8.备件"}, 
+         {"key": "calibrationSupplier", "value":"9.校准服务提供商"}, 
+         {"key": "toolSpecification", "value":"10.工具规格"}, 
+         {"key": "procurementPrice", "value":"11.采购价格"}],
+    filters:[{"key":"filterC", "value": "1.过滤1"}]
  };
--- tableTree/js/tableTree.js
+++ tableTree/js/tableTree.js
@@ -1,34 +1,19 @@
 var tableTree = function(){ 
-    var headers = [];
     var tableH = document.getElementById("tableH");
-    var cFilter = document.getElementById("filterC");
-    var containers = [tableH,cFilter]; 
+    var cFilter = document.getElementById("filterC"); 
     var tableC = document.getElementById("tableC"); 
+    var startType, endType;
+    // 有三种状态，A->B, B->A, B->B
     var flag = false;
+    var headers = [];
+    var filters = [];  
 
-    /** 初始化数据 */
-    function initHeader(){
-        // 初始化表头显示数据
-        var str = "";
-        for(var i in vTableData.tableHeader){
-            str += "<div class='item' id='" + vTableData.tableHeader[i] + "' data-name='"+ vTableData.tableHeader[i] +"' draggable='true' >" + i + "</div>"
-        }
-        tableH.innerHTML = str;
-        str = "";
-        for(var i = 0; i > vTableData.data.length; i++){
-            str += "<div>"
-            for(var j in vTableData.data[i]){
-                str += "";
-            }
-            str += "</div>"
-        }
-        tableC.innerHTML = str;
-   
-        // 表头拖动事件
-        $('div.item').mousedown(function(){
-            console.log("onmouseDown");
-            let startIndex = $(this).index();
-            let endIndex;
+    function itemMouseDown(){ 
+        var startIndex = $(this).data("id");
+        var endIndex;
+        // 鼠标按下时获取当前的类型
+        startType = $(this).data("type");
+        // 状态3 B内拖动 
             // 鼠标拖动在表头上
             flag = true;
             $('#info').css({
@@ -50,14 +35,11 @@ var tableTree = function(){
                     return false;
                 } 
             });
-            // 拖动到表头div 即触发元素交换
+            // 拖动到表头div 即触发元素交换 B->B
             $('#tableH div').mouseenter(function() {
                 endIndex = $(this).index();
-                if (endIndex == startIndex) {
-                    $('#triangle').css('display', 'none');
-                } else {
-                    $('#triangle').css('display', 'block');
-                }
+                endType = "header";
+                $('#triangle').css('display', 'block'); 
                 var offsetW = 0;
                 var preTd = $(this).prevAll();
                 $.each(preTd, function(id, item) {
@@ -71,75 +53,76 @@ var tableTree = function(){
                     'left': offsetW + 4
                 });
             });
-            // 交换元素
-            $(document).mouseup(function(e) {
-                flag = false; 
-                var e = e || window.event;
-                var x = e.clientX;
-                var y = e.clientY;  
-                console.log("x=" + x + " ;y="+y); 
-                console.log("$(cFilter).width()=" + $(cFilter).width() + " ;$(cFilter).height()="+ $(cFilter).height()); 
-                // 交换数据
-                if(x < $(cFilter).width() && y < $(cFilter).height()){
-                    // 判断位置 在筛选区域 
-                    $('#filterC').append($('#tableH div.item:eq(' + startIndex + ')').clone(true));
-                    $('#tableH div.item:eq(' + startIndex + ')').remove();
-                } else if(x < $(cFilter).width() && y > $(cFilter).height()){
-                    // 判断位置 在表头区域  
-                    // $('#div.item:eq(' + endIndex + ')').after($('#div.item:eq(' + startIndex + ')').clone(true));
-                    // $('div.item:eq(' + endIndex + ')').before($('#filterC div.item:eq(' + startIndex + ')').clone(true));
-                    // $('#filterC div.item:eq(' + startIndex + ')').remove();
-
-                    // 位置互换
-                    if (endIndex < startIndex) {
-                        // $('#mainTable tr:eq(' + i + ') td:eq(' + endIndex + ')').before($('#mainTable tr:eq(' + i + ') td:eq(' + startIndex + ')').clone(true));
-                        // $('#mainTable tr:eq(' + i + ') td:eq(' + (startIndex + 1) + ')').remove();
-                        $('div.item:eq(' + endIndex + ')').before($('div.item:eq(' + startIndex + ')').clone(true));
-                        $('div.item:eq(' + (startIndex + 1) + ')').remove();
-                    } else if (endIndex > startIndex) { 
-                        // $('#mainTable tr:eq(' + i + ') td:eq(' + endIndex + ')').after($('#mainTable tr:eq(' + i + ') td:eq(' + startIndex + ')').clone(true));
-                        // $('#mainTable tr:eq(' + i + ') td:eq(' + (startIndex) + ')').remove();
-                        $('div.item:eq(' + endIndex + ')').after($('div.item:eq(' + startIndex + ')').clone(true));
-                        $('div.item:eq(' + (startIndex) + ')').remove(); 
-                    } 
-                }
-
-                $('#triangle').css('display', 'none');
-                $('#info').css({'display': 'none'});
-
-                $(document).unbind("mousemove");
-                $(document).unbind("mouseup");
-                $('#tableH div').unbind("mouseenter");
-            });
-            // 拖入元素到筛选容器
-            $("#filterC").mouseenter(function(){
-                
+            // 拖动到过滤器 即触发 B-> A 把header里的数据元素存放在A
+            $('#filterC').mouseenter(function(){
+                endType = "filter";
+                // 追加到A最后 
             })
-        });
+            // 交换元素
+        $(document).mouseup(function(e) {
+            flag = false;  
+            console.log("startIndex=" + startIndex + "endIndex=" + endIndex); 
+            // B->B 第三种B内的移动
+            if(startType == endType && startType == "header"){ 
+                headers.splice(endIndex, 0, headers.splice(startIndex, 1)[0]);  
+            // B->A 第二种移动 
+            }else if(startType == "header" && endType == "filter"){
+                filters.push(headers.splice(startIndex, 1)[0]);  
+            // A->B 第一种移动
+            }else if(startType == "filter" && endType == "header"){
+                headers.splice(endIndex+1, 0, filters.splice(startIndex, 1)[0]);   
+            }
+            
+            $('div.item').unbind("mousedown");
+            initDisplay(headers,filters);
 
+            $('#triangle').css({'display': 'none'});
+            $('#info').css({'display': 'none'});
+
+            // 表头拖动事件 
+            $(document).unbind("mousemove");
+            $(document).unbind("mouseup");
+            $('#tableH div').unbind("mouseenter"); 
+        });  
+    }
+
+    function initDisplay(h,f){
+        // 初始化表头显示数据
+        var str = "";
+        headers = h;
+        for(var i = 0; i < headers.length; i++){
+            str += "<div class='item' id='" + headers[i].key + "' data-type='header' data-id='"+ i +"' draggable='true' >" + headers[i].value + "</div>"
+        }
+        tableH.innerHTML = str;
+        // 初始化过滤器
+        str = ""; 
+        filters = f;
+        for(var i = 0; i < filters.length; i++){
+            str += "<div class='item' id='" + filters[i].key + "' data-type='filter' data-id='"+ i +"' draggable='true' >" + filters[i].value + "</div>"
+        }
+        cFilter.innerHTML = str;
+        // for(var i = 0; i > vTableData.data.length; i++){
+        //     str += "<div>"
+        //     for(var j in vTableData.data[i]){
+        //         str += "";
+        //     }
+        //     str += "</div>"
+        // }
+        // tableC.innerHTML = str;
+
+        // 表头拖动事件
+        $('div.item').mousedown(itemMouseDown);
+    } 
+
+    /** 初始化数据 */
+    function initHeader(){
+        initDisplay(vTableData.tableHeader,vTableData.filters); 
+         
         // 初始化arrow的位置
         var h = $('#tableH div').height();
         $('.arrow-up').css({
             'margin-top': h
-        });
-
-
-        /** 表头拖拽事件 */  
-        $('#tableH div').dragstart = function(event){
-            var e = event || window.event;
-            e.dataTransfer.setData('text',e.target.id);
-            console.log("dragstart:" + e.target.id);
-        }
-        // header.ondragend = function(){
-        //     console.log("dragend");
-        // }
-
-        // header.ondragenter = function(){
-        //     console.log("ondragenter");
-        // } 
-        // header.ondragleave = function(){
-        //     console.log("ondragleave");
-        // } 
+        }); 
     } 
 
     return { 
